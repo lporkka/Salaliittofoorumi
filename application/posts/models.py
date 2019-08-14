39e5bed72afabe892053d695739a4a7f7097f444
Lauri <lauri.porkka@hotmail.com>
2019-08-14 20:52:49 +0300

    foreignkey fix

--- application/posts/models.py
+++ application/posts/models.py
@@ -7,5 +7,9 @@ class Post(db.Model):
 
     content = db.Column(db.String, nullable=False)
 
+    account_id = db.Column(db.Integer,
+                           db.ForeignKey('account.id'),
+                           nullable=False)
+
     def __init__(self, content):
         self.content = content
